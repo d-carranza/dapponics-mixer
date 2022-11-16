a713a0d9104b2c25797800fa5006eb8b1fb0d47e
d-carranza <diego.carranza@protonmail.com>
2022-11-16 12:41:41 +0900

    formatting

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import * as React from 'react'
+import * as React from "react";
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
@@ -11,20 +11,19 @@ function App() {
     <ChakraProvider>
       <TheRestOfYourApplication />
     </ChakraProvider>
-  )
+  );
 }
 
 // npm run dev
 
-
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return <h1>Testing react code</h1>;
-    }
+  render() {
+    return <h1>Testing react code</h1>;
+  }
 }
 
 const appDiv = document.getElementById("app");
