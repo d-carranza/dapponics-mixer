d90ad50c92b4d264fbe65af5a797bcbe060b0d31
d-carranza <diego.carranza@protonmail.com>
2022-11-29 03:52:02 +0900

    TODO comments updated

--- app/src/components/Buttons.js
+++ app/src/components/Buttons.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-function Buttons() {
+// TODO: Save Changes is a form that fetches the state to the backend
+// TODO: Create Collection is ../../static/app/logic
+
+function Buttons(props) {
+  const state = props.state;
+  const setState = props.setState;
+
   return (
     <div className="buttons">
       <button className="app-btn">Save Changes</button>
