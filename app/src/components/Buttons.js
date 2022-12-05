21b48078e818f7a085af72791407b45280385a36
d-carranza <diego.carranza@protonmail.com>
2022-12-05 17:06:37 +0900

    Save button filters null values and rarity sum

--- app/src/components/Buttons.js
+++ app/src/components/Buttons.js
@@ -8,15 +8,55 @@ function Buttons(props) {
   const setState = props.setState;
 
   async function saveChanges() {
-    const request = {
-      method: "POST",
-      body: JSON.stringify({ ...state }),
-    };
+    // Filter user inputs before sending data to the database
 
-    // Fetch POST request
-    const response = await fetch("/save", request);
-    const result = await response.json();
-    console.info(result);
+    // Check null inputs
+    let nullValues = false;
+    for (let attribute of state["attributes"]) {
+      if (attribute["trait_type"] == "") {
+        nullValues = true;
+      }
+      for (let trait of attribute["traits"]) {
+        if (trait["img"] == "" || trait["value"] == "" || trait["rarity"] == "")
+          nullValues = true;
+      }
+    }
+    if (nullValues == true) {
+      alert("Some fields are empty");
+    }
+
+    // Check Rarity total
+    let rarityIsValid = false;
+    let total = 0;
+    console.log(total);
+    for (let attribute of state["attributes"]) {
+      for (let trait of attribute["traits"]) {
+        const rarity = parseInt(trait["rarity"]);
+        if (typeof rarity == "number") {
+          total += rarity;
+          console.log(total);
+        }
+      }
+    }
+    if (total == 100) {
+      rarityIsValid = true;
+    }
+    if (rarityIsValid == false) {
+      alert("Same trait type rarities must add up 100");
+    }
+
+    // If fields are valid then fetch the state to the backend
+    if (nullValues == false && rarityIsValid == true) {
+      const request = {
+        method: "POST",
+        body: JSON.stringify({ ...state }),
+      };
+
+      // Fetch POST request
+      const response = await fetch("/save", request);
+      const result = await response.json();
+      console.info(result);
+    }
   }
 
   return (
