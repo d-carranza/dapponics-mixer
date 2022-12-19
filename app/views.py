572b5b0d7345ffa601beedc1f1943b4e2031b846
d-carranza <diego.carranza@protonmail.com>
2022-12-20 02:36:56 +0900

    bug fixes

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import json
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
